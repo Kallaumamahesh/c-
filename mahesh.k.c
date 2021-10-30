f02dab3467d3f9ef68f31ee134ce9247f1a3dd3b
Kallaumamahesh <93186608+Kallaumamahesh@users.noreply.github.com>
2021-10-30 00:00:39 -0700

    Add files via upload

--- mahesh.k.c
+++ mahesh.k.c
@@ -1,15 +1,9 @@
+/* Adding two integers in c*/
 #include<stdio.h>
 void main()
 {
-	//variables
-	float num1, num2, sum;
-	
-	//instialization
-	num1 = 2.2;
-	num2 = 3.61;
-	
-	//calculation
-	sum = num1+num1;
+
+	float num1=2.2, num2=3.61, sum=num1+num2;
 	
 	//output
 	printf("sum of %f and %f is %f",num1, num2,sum);
