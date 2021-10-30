23d538b2055d508df4521598110e1208c023eed4
Kallaumamahesh <93186608+Kallaumamahesh@users.noreply.github.com>
2021-10-29 23:43:35 -0700

    Add files via upload

---
+++ mahesh.k.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//variables
+	float num1, num2, sum;
+	
+	//instialization
+	num1 = 2.2;
+	num2 = 3.61;
+	
+	//calculation
+	sum = num1+num1;
+	
+	//output
+	printf("sum of %f and %f is %f",num1, num2,sum);
+	
+	
+}
